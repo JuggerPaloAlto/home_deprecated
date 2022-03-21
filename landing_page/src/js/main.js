b9c17e08ef31792cc89828b53094d97957c3cfb8
Calvin Wong <ctwong958@berkeley.edu>
2022-03-20 19:29:12 -0700

    I got the freaking animations lesgo

--- landing_page/src/js/main.js
+++ landing_page/src/js/main.js
@@ -5,6 +5,7 @@ var open_credits = document.getElementById("open-credits");
 var credits = document.getElementById("credits");
 
 function fadeIn(elem, duration=15) {
+    elem.style.display="block";
     var opacity = 0;
     elem.style.opacity = 0;
     var intervalID = setInterval(function() {
@@ -13,12 +14,12 @@ function fadeIn(elem, duration=15) {
             elem.style.opacity = opacity;
         } else {
             clearInterval(intervalID);
-            elem.style.display="block";
         }
     }, duration);
 }
 
 function fadeOut(elem, duration=15) {
+    elem.style.display="block";
     var opacity = 1;
     elem.style.opacity = 1;
     var intervalID = setInterval(function() {
@@ -48,3 +49,15 @@ function show_credits() {
 
 open_credits.onclick = show_credits;
 close_credits.onclick = hide_credits;
+
+function sleep(milliseconds) {  
+    return new Promise(resolve => setTimeout(resolve, milliseconds));  
+ }  
+ async function do_starting_anim(elem, delay, duration=35, anim=fadeIn) {  
+    await sleep(delay);  
+    anim(elem, duration)
+ }  
+ do_starting_anim(header, 7000);
+ do_starting_anim(document.getElementById("summary1"), 16000, 40)
+ do_starting_anim(document.getElementById("summary2"), 24000, 40)
+ do_starting_anim(footer, 25000);
