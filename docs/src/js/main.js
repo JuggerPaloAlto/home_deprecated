b1e02ddc754854476847e0889b5e57eff43b60df
Calvin Wong <ctwong958@berkeley.edu>
2022-03-24 23:26:05 -0700

    Add ability to click to skip through intro

--- docs/src/js/main.js
+++ docs/src/js/main.js
@@ -49,15 +49,48 @@ function show_credits() {
 
 open_credits.onclick = show_credits;
 close_credits.onclick = hide_credits;
+ 
+
+function* show_intro_message() {
+    fadeIn(header, 35);
+    yield;
+    fadeIn(document.getElementById("summary1"), 40);
+    yield;
+    fadeIn(document.getElementById("summary2"), 40);
+    yield;
+    fadeIn(footer, 35);
+    document.removeEventListener("click", intro_iter_next);
+    return;
+}
+
+function* timed_intro() {
+    do_starting_anim(7000);
+    yield;
+    do_starting_anim(9000);
+    yield;
+    do_starting_anim(8000);
+    yield;
+    do_starting_anim(1000);
+    return;
+}
+
+const intro_iter = show_intro_message();
+const intro_timed_iter = timed_intro();
 
 function sleep(milliseconds) {  
     return new Promise(resolve => setTimeout(resolve, milliseconds));  
- }  
- async function do_starting_anim(elem, delay, duration=35, anim=fadeIn) {  
+ }
+
+ async function do_starting_anim(delay) {  
     await sleep(delay);  
-    anim(elem, duration)
- }  
- do_starting_anim(header, 7000);
- do_starting_anim(document.getElementById("summary1"), 16000, 40)
- do_starting_anim(document.getElementById("summary2"), 24000, 40)
- do_starting_anim(footer, 25000);
+    intro_iter.next();
+    intro_timed_iter.next()
+ }
+
+intro_timed_iter.next();
+
+function intro_iter_next() {
+    intro_iter.next();
+}
+
+document.addEventListener('click', intro_iter_next);
