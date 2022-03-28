82190a08226796425adac261a1fd3585764c1ba6
Calvin Wong <ctwong958@berkeley.edu>
2022-03-27 18:47:53 -0700

    sidenav bar now closes when you click off it

--- docs/src/js/sidenav.js
+++ docs/src/js/sidenav.js
@@ -1,14 +1,17 @@
 const sidenav = document.getElementById("sidenav");
 const main = document.getElementById("main");
 
+
 function openNav() {
     sidenav.style.width = "250px";
     main.style.marginLeft = "250px";
     sidenav.style.left = "0";
-  }
-  
-  function closeNav() {
+    setTimeout(() => main.onclick=closeNav, 100);
+}
+
+function closeNav() {
     sidenav.style.width = "0";
     main.style.marginLeft = "0";
     sidenav.style.left = "-250px";
-  }
+    main.onclick=null;
+}
