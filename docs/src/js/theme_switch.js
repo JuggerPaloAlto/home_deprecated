7e8b4a5e8b39fed26164fae4b322428507689cb3
Calvin Wong <ctwong958@berkeley.edu>
2022-03-29 13:17:53 -0700

    Add and cleanup link coloring/animations

--- docs/src/js/theme_switch.js
+++ docs/src/js/theme_switch.js
@@ -1,6 +1,8 @@
 function toggle_theme() {
     // $("body").toggleClass("light-theme");
     $(".sidenav").toggleClass("light-theme");
-    $(".sidenav a").toggleClass("light-theme");
+    $("a").toggleClass("light-theme");
     $(".collapse").toggleClass("light-theme");
+    $(".parallax__layer--top .title").toggleClass("light-theme");
+    
 }
