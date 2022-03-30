e315c5c7d65aaa1349733a5bb76a5365509860d2
Calvin Wong <ctwong958@berkeley.edu>
2022-03-29 22:51:26 -0700

    Dark/light theme displays based on current theme

--- docs/src/js/theme_switch.js
+++ docs/src/js/theme_switch.js
@@ -1,8 +1,16 @@
+var light_mode_on = false;
+
 function toggle_theme() {
     // $("body").toggleClass("light-theme");
     $(".sidenav").toggleClass("light-theme");
     $("a").toggleClass("light-theme");
     $(".collapse").toggleClass("light-theme");
     $(".parallax__layer--top .title").toggleClass("light-theme");
+    light_mode_on = !light_mode_on;
+    if (light_mode_on) {
+        $(".theme-switch").text("Dark Mode");
+    } else {
+        $(".theme-switch").text("Light Mode");
+    }
     
 }
